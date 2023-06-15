7848d211a32b89a5d5f827f2a5d4c5eda0e0ba23
jonathasluis <jonathas_sousasgs@hotmail.com>
2023-06-14 21:36:21 -0300

    primeira secao do primeiro curso da formacao.

---
+++ 01-JavaScript-tipos-variaveis-e-funcoes/01-tiposPrimitivos/01-type_number.js
@@ -0,0 +1,21 @@
+// tipo Number
+
+const meuNumero = 3;
+const num1 = 1;
+const num2 = 2;
+
+const opMat = num1 * num2;
+
+console.log(opMat);
+
+// ponto flutuante
+
+const numPf1 = 3.3;
+const numPf2 = .5;
+const opMat2 = numPf1 / numPf2;
+console.log(opMat2);
+
+// NaN -> Not a Number
+
+const alura = 'Alura';
+console.log(alura*2);
---
+++ 01-JavaScript-tipos-variaveis-e-funcoes/01-tiposPrimitivos/03-type_boolean.js
@@ -0,0 +1,5 @@
+const num1 = 5;
+const num2 = 5;
+let teste = true;
+
+console.log(num1 === num2);
